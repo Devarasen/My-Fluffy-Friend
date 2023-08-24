133b47525c5640a5650ed113bbc048a8f0d31893
Devarasen <devarasen@gmail.com>
2023-08-24 19:49:51 +1000

    controllers_setup_DS

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const path = require("path");
 
 router.use("/api", apiRoutes);
+
+router.get("/", async (req, res) => {
+  res.render("home", { title: "Home Page" });
+});
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>");
