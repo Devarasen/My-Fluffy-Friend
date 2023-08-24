99ea9b9f6055d33e8f6d34ba964dc17e1a1f7773
Em3L <martink_lopez@yahoo.com>
2023-08-24 21:06:32 +1000

    updated index js file code

--- models/index.js
+++ models/index.js
@@ -1,15 +1,28 @@
 const User = require('./User');
-const Project = require('./pet');
+const Pet = require('./Pet'); 
+const Category = require('./Category'); 
+const AdoptionDate = require('./AdoptionDate'); 
+const AdoptionCenter = require('./AdoptionCenter'); 
 
-User.hasMany(Project, {
+User.hasMany(Pet, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Pet.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+Pet.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
+Pet.hasOne(AdoptionDate, {
+  foreignKey: 'pet_id'
+});
 
+Pet.hasOne(AdoptionCenter, {
+  foreignKey: 'pet_id'
+});
+
+module.exports = { User, Pet, Category, AdoptionDate, AdoptionCenter };
