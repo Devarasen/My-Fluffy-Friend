3fe1153daa98bbe3614bee80bbbf31ca3fdc205f
Devarasen <devarasen@gmail.com>
2023-08-24 20:35:36 +1000

    pet routes get all

--- controllers/api/petRoutes.js
+++ controllers/api/petRoutes.js
@@ -1,3 +1,14 @@
 const router = require("express").Router();
+const { Pet } = require("../../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const PetData = await Pet.findAll();
+
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
