870b3053005634cddd38d7fcca6fe985d4848940
Devarasen <devarasen@gmail.com>
2023-08-24 20:23:30 +1000

    update

--- controllers/index.js
+++ controllers/index.js
@@ -4,12 +4,8 @@ const path = require("path");
 
 router.use("/api", apiRoutes);
 
-router.get("/", async (req, res) => {
-  res.render("home", { title: "Home Page" });
-});
-
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>");
-});
+// router.use((req, res) => {
+//   res.send("<h1>Wrong Route!</h1>");
+// });
 
 module.exports = router;
