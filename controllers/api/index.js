c98dee28ece5d52586be70de0e234cd42fb8324f
Devarasen <devarasen@gmail.com>
2023-08-26 13:07:29 +1000

    route updates

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
-const userRoutes = require("./userRoutes");
 const petRoutes = require("./petRoutes");
 const adoptionCenterRoutes = require("./adoptionCenterRoutes");
 
-router.use("/users", userRoutes);
 router.use("/petRoutes", petRoutes);
 router.use("/adoptionCenterRoutes", adoptionCenterRoutes);
 
