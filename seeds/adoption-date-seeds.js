bf7dc38502fa52efefadd6039b068403a3ca9548
Thiago Dantas <thiagoo.dantas11@gmail.com>
2023-08-26 12:07:45 +1000

    code added to index.js in models folder and more data added to database seeds

--- seeds/adoption-date-seeds.js
+++ seeds/adoption-date-seeds.js
@@ -2,9 +2,23 @@ const { AdoptionDate } = require('../models');
 
 const adoptionDateData = [
     {
-      id: 1,
       adoption_center_id: 1,
+      pet_id: 5,
+      available_from: Datatype.Date,  
+    },
+    {
+      adoption_center_id: 2,
       pet_id: 2,
+      available_from: Datatype.Date,  
+    },
+    {
+      adoption_center_id: 3,
+      pet_id: 3,
+      available_from: Datatype.Date,  
+    },
+    {
+      adoption_center_id: 4,
+      pet_id: 8,
       available_from: Datatype.Date,  
     },
 ];
