5d1a775d52ee740c635a8cc7b5bb0e68b7bde423
MDSLVA <marcelosilvaa92@gmail.com>
2023-08-29 21:17:57 +1000

    update routes for handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,52 @@
+// const router = require("express").Router();
+// const { Pet, Category, AdoptionCenter } = require("../models");
+
+// router.get("/", async (req, res) => {
+//   return res.render("home");
+// });
+
+// router.get("/petRoutes", async (req, res) => {
+//   try {
+//     const petData = await Pet.findAll({
+//       include: [Category, AdoptionCenter],
+//     });
+//     const formattedPetData = petData.map((pet) => pet.get({ plain: true }));
+//     res.render("pets", { petData: formattedPetData });
+//   } catch (error) {
+//     res.status(500).json(error);
+//   }
+// });
+
+// router.get("/petRoutes/:id", async (req, res) => {
+//   try {
+//     const petData = await Pet.findByPk(req.params.id, {
+//       include: [Category, AdoptionCenter],
+//     });
+//     const formattedPetData = petData.get({ plain: true });
+//     res.render("pet", { petData: formattedPetData });
+//   } catch (error) {
+//     res.status(500).json(error);
+//   }
+// });
+
+// module.exports = router;
+
 const router = require("express").Router();
 const { Pet, Category, AdoptionCenter } = require("../models");
 
+
 router.get("/", async (req, res) => {
   return res.render("home");
+});
+
+
+router.get("/login", async (req, res) => {
+  return res.render("login");
+});
+
+
+router.get("/post-adoption", async (req, res) => {
+  return res.render("postAdoption"); 
 });
 
 router.get("/petRoutes", async (req, res) => {
@@ -16,6 +60,7 @@ router.get("/petRoutes", async (req, res) => {
     res.status(500).json(error);
   }
 });
+
 
 router.get("/petRoutes/:id", async (req, res) => {
   try {
