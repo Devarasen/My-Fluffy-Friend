852307cec0e4248582b426c67d1a63aa934d6bd9
Devarasen <devarasen@gmail.com>
2023-09-03 21:10:15 +1000

    fixed routes for error handling

--- controllers/api/categoryRoutes.js
+++ controllers/api/categoryRoutes.js
@@ -1,23 +1,31 @@
 const { Category } = require("../../models");
 const router = require("express").Router();
 
+// Get all categories
 router.get("/", async (req, res) => {
   try {
     const categoryData = await Category.findAll();
     res.status(200).json(categoryData);
   } catch (error) {
+    console.error("Error fetching all categories:", error);
     res.status(500).json(error);
   }
 });
 
-router.get("/", async (req, res) => {
-  const categoryData = await Category.findByPk();
-  res.render("main", { categoryData });
-});
-
+// Get a specific category by ID
 router.get("/:id", async (req, res) => {
-  const categoryData = await Category.findByPk(req.params.id);
-  res.status(200).json(categoryData);
+  try {
+    const categoryData = await Category.findByPk(req.params.id);
+    if (!categoryData) {
+      return res
+        .status(404)
+        .json({ message: "No category found with this ID!" });
+    }
+    res.status(200).json(categoryData);
+  } catch (error) {
+    console.error(`Error fetching category with ID ${req.params.id}:`, error);
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,9 @@ const homeRoutes = require("./home-routes.js");
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
 
+// Catch-all route handler for any other unrecognized requests
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>");
+  res.status(404).type("text/html").send("<h1>Wrong Route!</h1>");
 });
 
 module.exports = router;
